3f2d887186d1ac99fb6805f47e2a5672c4325393
tob <tob@nice.eu>
2021-01-30 23:05:03 +0100

    also fix docker ps command for ci_health.sh

--- ci_health.sh
+++ ci_health.sh
@@ -2,11 +2,11 @@
 set -x
 HEALTH="notok" && counter=0
 while [[ -n ${HEALTH} ]] && [ $counter -le 120 ]; do
-  HEALTH=$(docker ps -q --filter --filter health=starting --filter health=unhealthy --filter health=none)
+  HEALTH=$(docker ps -q --filter health=starting --filter health=unhealthy --filter health=none)
   sleep 1
   counter=$(( $counter + 1 ))
   echo $counter
-  docker ps -q --filter --filter health=starting --filter health=unhealthy --filter health=none
+  docker ps -q --filter health=starting --filter health=unhealthy --filter health=none
   if [ $counter -eq 120 ]; then
     echo $counter
     docker-compose logs
