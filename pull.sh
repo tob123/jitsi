b27d069322e57f4977598b9bdc0d9c6f6227f6ae
tob <tob@nice.eu>
2021-01-31 08:39:38 +0100

    fix tagging to "conainer.previous" to allow container comparison

--- pull.sh
+++ pull.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
-for i in 2 3 4 5 6; do
+for i in 2 3 4 5; do
 CONTAINER=CONTAINER_${i}
 if docker-compose pull ${!CONTAINER}; then
   touch ${!CONTAINER}.compare
-  docker tag ${!CONTAINER} ${!CONTAINER}.previous; else
+  CONT_IMAGE="${BUILD_REGISTRY}/${PROJECT}/${!CONTAINER}:${JITSI_VERSION}"
+  docker tag ${CONT_IMAGE} ${!CONTAINER}.previous; else
   touch ${!CONTAINER}.scan
 fi
 done
