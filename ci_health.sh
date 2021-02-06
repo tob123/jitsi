b26d5bf3e3656c8d4e0795da5505354ee2825e69
tob <tob@nice.eu>
2021-02-06 13:31:47 +0100

    asdf

--- ci_health.sh
+++ ci_health.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -x
 HEALTH="notok" && counter=0
 while [[ -n ${HEALTH} ]] && [ $counter -le 120 ]; do
   HEALTH=$(docker ps -q --filter health=starting --filter health=unhealthy --filter health=none)
   sleep 1
   counter=$(( $counter + 1 ))
   echo $counter
-  docker ps -q --filter health=starting --filter health=unhealthy --filter health=none
+  docker ps ---filter health=starting --filter health=unhealthy --filter health=none
   if [ $counter -eq 120 ]; then
     echo $counter
     docker-compose logs
