b989d558a78d70b148dc993286a99095aaf6eca5
tob <tob@nice.eu>
2021-02-17 21:34:05 +0100

    check

--- base/build.sh
+++
@@ -1,6 +0,0 @@
-export S6_VERSION=v2.1.0.2
-export S6_VERSION_MAJOR=v2.1
-sudo -E docker-compose build s6-base
-sudo -E docker tag tob123/s6-base:${S6_VERSION} tob123/s6-base:${S6_VERSION_MAJOR}
-sudo -E docker tag tob123/s6-base:${S6_VERSION} tob123/s6-base
-sudo -E docker-compose build
