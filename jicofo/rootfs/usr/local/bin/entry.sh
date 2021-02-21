6defb4fbdc250bf93dadf97bfc908c210709a97d
tob <tob@nice.eu>
2021-02-21 13:52:57 +0100

    change to xmpp_websockets enabled by default and move jicofo to new config file

--- jicofo/rootfs/usr/local/bin/entry.sh
+++ jicofo/rootfs/usr/local/bin/entry.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export JAVA_SYS_PROPS="-Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=/ -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=config -Djava.util.logging.config.file=/config/logging.properties"
+#export JAVA_SYS_PROPS="-Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=/ -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=config -Djava.util.logging.config.file=/config/logging.properties"
+export JAVA_SYS_PROPS="-Djava.util.logging.config.file=/config/logging.properties -Dconfig.file=/config/jicofo.conf"
 export DAEMON=/usr/share/jicofo/jicofo.sh
 export DAEMON_DIR=/usr/share/jicofo/
-export DAEMON_OPTS="--domain=meet.jitsi --host=xmpp.meet.jitsi --user_name=focus --user_domain=auth.meet.jitsi"
+#export DAEMON_OPTS="--domain=meet.jitsi --host=xmpp.meet.jitsi --user_name=focus --user_domain=auth.meet.jitsi"
+export DAEMON_OPTS="--secret=$JICOFOT_SECRET --domain=meet.jitsi --host=xmpp.meet.jitsi --user_name=focus --user_domain=auth.meet.jitsi --user_password=$JICOFO_AUTH_PASSWORD"
 exec "$@"
 
