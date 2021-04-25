5953e43e4447f03d504c6be5d02bd50a86d64607
tob123 <tobias@appelo.org>
2021-04-25 10:33:43 +0200

    add stable tags for coturn

--- get_tags.sh
+++ get_tags.sh
@@ -30,6 +30,12 @@ for i in 2 3 4 5; do
   TAG_STRING="$TAG_STRING $TAG_STRING_ADD"
 done
 }
+set_tag_stable_turn () {
+  CONTAINER="CONTAINER_6"
+  CONT_IMAGE="${BUILD_REGISTRY}/${PROJECT}/${!CONTAINER}"
+  TAG_STRING_ADD="--set ${!CONTAINER}.tags=${CONT_IMAGE}:${COTURN_VERSION} --set ${!CONTAINER}.tags=${CONT_IMAGE}:${TAG_STRING_STABLE}"
+  TAG_STRING="$TAG_STRING $TAG_STRING_ADD"
+}
 MAJOR_TAG=$JITSI_VERSION
 until [[ -z "$MAJOR_TAG"  ]];do
   if MAJOR_TAG=`echo $MAJOR_TAG | sed 's/\.[0-9]*$//'`;then
@@ -41,4 +47,5 @@ until [[ -z "$MAJOR_TAG"  ]];do
 done
 set_tag_min
 set_tag_stable
+set_tag_stable_turn
 export TAG_STRING
