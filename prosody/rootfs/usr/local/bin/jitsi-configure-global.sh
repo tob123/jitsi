1243dd556bf59225ffd068762d09f1d181ccc469
tob123 <tobias@appelo.org>
2021-04-28 22:39:06 +0200

    re-apply commit regarding external_services and fix port syntax

--- prosody/rootfs/usr/local/bin/jitsi-configure-global.sh
+++ prosody/rootfs/usr/local/bin/jitsi-configure-global.sh
@@ -10,10 +10,10 @@ if [[ $ENABLE_TURN -eq 1 ]]; then
 cat <<EOF
 external_service_secret = "${TURN_PASSWORD}";
 external_services = {
-  { type = "stun", host = "${TURN_DOMAIN}", port = "3478" },
-  { type = "turn", host = "${TURN_DOMAIN}", port = "3478", transport = "udp", secret = true, ttl = 86400, algorithm = "turn" },
-  { type = "turns", host = "${TURN_DOMAIN}", port = "5349", transport = "tcp", secret = true, ttl = 86400, algorithm = "turn" },
-  { type = "turns", host = "${TURN_DOMAIN}", port = "443", transport = "tcp", secret = true, ttl = 86400, algorithm = "turn" }
+  { type = "stun", host = "${TURN_DOMAIN}", port = 3478 },
+  { type = "turn", host = "${TURN_DOMAIN}", port = 3478, transport = "udp", secret = true, ttl = 86400, algorithm = "turn" },
+  { type = "turns", host = "${TURN_DOMAIN}", port = 5349, transport = "tcp", secret = true, ttl = 86400, algorithm = "turn" },
+  { type = "turns", host = "${TURN_DOMAIN}", port = 443, transport = "tcp", secret = true, ttl = 86400, algorithm = "turn" }
 };
 EOF
 fi
