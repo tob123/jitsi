6f6cd11758ddae0b3e012d58d415a2e7df35d63a
tob123 <tobias@appelo.org>
2021-10-08 00:38:02 +0200

    asdf

--- getcoturnversion.sh
+++ getcoturnversion.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://git.alpinelinux.org/aports/plain/community/coturn/APKBUILD?h=3.12-stable | grep pkgver\= | awk -F '=' {'print $2'}
+curl -s https://git.alpinelinux.org/aports/plain/community/coturn/APKBUILD?h=3.14-stable | grep pkgver\= | awk -F '=' {'print $2'}
