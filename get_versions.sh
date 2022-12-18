c89fc8c3e598bd90af49dee7053127242dfce394
tob123 <tobias@appelo.org>
2022-12-18 13:31:57 +0100

    update alpine to 317

--- get_versions.sh
+++ get_versions.sh
@@ -4,5 +4,5 @@ JITSI_VERSION_LATEST=`echo -ne "$JITSI_VERSION_LIST" | tail -1`
 JITSI_VERSION_GITHUB_LATEST=`echo $JITSI_VERSION_LATEST | sed 's/-[0-9]*$//' | awk -F . {'print $NF'}`
 JITSI_SEM_LATEST=`echo $JITSI_VERSION_LATEST | sed s/-/./`
 JITSI_LATEST_MJ=`echo $JITSI_SEM_LATEST | sed 's/\.[0-9]$//'`
-COTURN_VERSION=`curl -s https://git.alpinelinux.org/aports/plain/community/coturn/APKBUILD?h=3.15-stable | grep ^pkgver | awk -F '=' {'print $2'}`
+COTURN_VERSION=`curl -s https://git.alpinelinux.org/aports/plain/community/coturn/APKBUILD?h=3.17-stable | grep ^pkgver | awk -F '=' {'print $2'}`
 COTURN_VERSION_MJ=`echo $COTURN_VERSION | awk -F . {'print $1"."$2'}`
