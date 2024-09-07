4fcb05be220fa1803fd3ea2b8a8185123ef7a7b1
tobias <tobias@asdf.appelo.org>
2024-09-07 13:58:44 +0200

    disable comparing

--- compare.sh
+++ compare.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -x
+set -e
 for i in 2 3 4 5; do
 CONTAINER=CONTAINER_${i}
 if [[ -f ${!CONTAINER}.compare ]]; then
   CONT_IMAGE="${BUILD_REGISTRY}/${PROJECT}/${!CONTAINER}:${JITSI_VERSION}"
   container-diff diff -v info -t file -json daemon://${!CONTAINER}.previous daemon://${CONT_IMAGE} | jq '.[].Diff[] | select ((.!=null))' > contdifftmp || exit 1
   if [[ -s contdifftmp ]]; then
-    cat contdifftmp
-    for j in $(cat contdifftmp | jq -r .[].Name); do echo $j; if ! grep $j comp_wl;then echo push;touch ${!CONTAINER}.scan;fi;done
+#    cat contdifftmp
+    for j in $(cat contdifftmp | jq -r .[].Name); do  if ! grep $j comp_wl;then touch ${!CONTAINER}.scan;fi;done
   fi
 fi
 done
@@ -15,7 +15,7 @@ if [[ -f ${CONTAINER_6}.compare ]]; then
  CONT_IMAGE="${BUILD_REGISTRY}/${PROJECT}/${CONTAINER_6}:${COTURN_VERSION}"
   container-diff diff -v info -t file -json daemon://${CONTAINER_6}.previous daemon://${CONT_IMAGE} | jq '.[].Diff[] | select ((.!=null))' > contdifftmp || exit 1
   if [[ -s contdifftmp ]]; then
-    cat contdifftmp
-    for j in $(cat contdifftmp | jq -r .[].Name); do echo $j; if ! grep $j comp_wl;then echo push;touch ${CONTAINER_6}.scan;fi;done
+#    cat contdifftmp
+    for j in $(cat contdifftmp | jq -r .[].Name); do if ! grep $j comp_wl;then touch ${CONTAINER_6}.scan;fi;done
   fi
 fi
--- scan.sh
+++ scan.sh
@@ -2,17 +2,17 @@
 set -x
 for i in 2 3 4 5; do
 CONTAINER=CONTAINER_${i}
-if [[ -f ${!CONTAINER}.scan ]]; then
+#if [[ -f ${!CONTAINER}.scan ]]; then
   CONT_IMAGE="${BUILD_REGISTRY}/${PROJECT}/${!CONTAINER}:${JITSI_VERSION}"
   trivy image --exit-code 1 -s "MEDIUM,HIGH,CRITICAL" --vuln-type library --ignore-unfixed ${CONT_IMAGE} || exit 1
   trivy image --exit-code 1 -s "MEDIUM,HIGH,CRITICAL" --vuln-type os --ignore-unfixed  ${CONT_IMAGE} || exit 1
   touch ${!CONTAINER}.push
-fi
+#fi
 done
-if [[ -f ${CONTAINER_6}.scan ]]; then
+#if [[ -f ${CONTAINER_6}.scan ]]; then
   CONT_IMAGE="${BUILD_REGISTRY}/${PROJECT}/${CONTAINER_6}:${COTURN_VERSION}"
   trivy image --exit-code 1 -s "MEDIUM,HIGH,CRITICAL" --vuln-type library --ignore-unfixed ${CONT_IMAGE} || exit 1
   trivy image --exit-code 1 -s "MEDIUM,HIGH,CRITICAL" --vuln-type os --ignore-unfixed  ${CONT_IMAGE} || exit 1
   touch ${CONTAINER_6}.push
-fi
+#fi
 
